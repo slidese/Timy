3b0f209ac046056c728053e1e9ac5b1d954e14e0
slide <mike@slide.se>
2013-03-26 12:27:10 +0100

    Added basic acknowledgements activity.

---
+++ src/se/slide/timy/AcknowledgementsActivity.java
@@ -0,0 +1,18 @@
+package se.slide.timy;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class AcknowledgementsActivity extends FragmentActivity {
+
+    /* (non-Javadoc)
+     * @see android.support.v4.app.FragmentActivity#onCreate(android.os.Bundle)
+     */
+    @Override
+    protected void onCreate(Bundle arg0) {
+        super.onCreate(arg0);
+        
+        //setContentView(R.id.acknowledgements);
+    }
+
+}
