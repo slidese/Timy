e22831364a41eef0e6c4b9ec44bade8149d306d2
slide <mike@slide.se>
2013-03-27 18:24:44 +0100

    Setting up the ack activity so that I can load messages from a string array (HTML formatted) and just stack them on top of each other. Would be an easy - general - approach to this type of screen.

--- src/se/slide/timy/AcknowledgementsActivity.java
+++ src/se/slide/timy/AcknowledgementsActivity.java
@@ -2,6 +2,9 @@ package se.slide.timy;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.util.TypedValue;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class AcknowledgementsActivity extends FragmentActivity {
 
@@ -12,7 +15,20 @@ public class AcknowledgementsActivity extends FragmentActivity {
     protected void onCreate(Bundle arg0) {
         super.onCreate(arg0);
         
-        //setContentView(R.id.acknowledgements);
+        setContentView(R.layout.activity_acknowledgements);
+        
+        LinearLayout layout = (LinearLayout) findViewById(R.id.acknowledgements);
+        
+        TextView line;
+        line = new TextView(this);
+        line.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 16);
+        line.setTextAppearance(this, android.R.attr.textAppearanceMedium);
+        
+        // Loop string array with HTML formatted strings
+        
+        line.setText("Used frame work version 1.0");
+        
+        layout.addView(line);
     }
 
 }
